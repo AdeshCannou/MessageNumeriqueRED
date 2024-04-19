9a5394374d8979ba3da3ae04e8a3e096e731c34c
Adesh Cannou <66022289+AdeshCannou@users.noreply.github.com>
2024-04-19 21:45:57 +0200

    update validate function

--- validate.py
+++ validate.py
@@ -1,6 +1,7 @@
 from enum import Enum
 import json
 import re
+import calendar
 
 class Genre(Enum):
     ACTION = "Action"
@@ -25,6 +26,11 @@ class TypeCouleur(Enum):
     BLANC = "Blanc"
     NOIR = "Noir"
 
+class TypeTailleFriandise(Enum):
+    M = "M"
+    L = "L"
+    XL = "XL"
+
 def validate_message(message_json, user_input):
     for key, value in message_json.items():
         print("Key: " + key)
@@ -36,13 +42,22 @@ def validate_message(message_json, user_input):
                     return False
             except ValueError:
                 return False
-        elif key == "isNumber" or key == "isQuantite":
-            number_pattern = r'\s\d+\s*'
-            if not re.search(number_pattern, user_input):
+        elif key == "isNumber":
+            parts = user_input.split()
+            if not any(part.isdigit() for part in parts):
                 return False
-        elif key == "isDate" or key == "isCreneau":
-            date_pattern = r'\b\d{1,2}/\d{1,2}/\d{4}\b'
-            if not re.search(date_pattern, user_input):
+        elif key == "isDate":
+            date_pattern = r'\b(0?[1-9]|[12]\d|3[01])/(0?[1-9]|1[0-2])/((19|20)\d{2})\b' 
+            parts = user_input.split()
+            date_found = False
+            for part in parts:
+                if re.match(date_pattern, part):
+                    day, month, year = map(int, part.split('/'))
+                    if day > calendar.monthrange(year, month)[1]:
+                        return False 
+                    date_found = True
+                    break
+            if not date_found:
                 return False
         elif key == "isCouleur":
             parts = user_input.split()
@@ -62,10 +77,20 @@ def validate_message(message_json, user_input):
             parts = user_input.split()
             if not any(part.lower() in [genre.value.lower() for genre in Genre] for part in parts):
                 return False
+        elif key == "isCreneau":
+            time_pattern = r'^([01]?[0-9]|2[0-3]):[0-5][0-9]$'
+            if not any(re.match(time_pattern, part) for part in user_input.split()):
+                return False
         elif key == "isType":
             parts = user_input.split()
             if not any(part.lower() in [type.value.lower() for type in TypeFriandise] for part in parts):
                 return False
+        elif key == "isQuantite":
+            parts = user_input.split()
+            taille_friandise_values = [str(taille.value) for taille in TypeTailleFriandise]
+            if not any(part in taille_friandise_values for part in parts):
+                return False
+
     return True
 
 
@@ -73,22 +98,17 @@ def validate_message(message_json, user_input):
 
 # Test pour isNumber
 # message_json = {"isNumber": True}
-# user_input = "ti 5d sdf 777 sdf  "
+# user_input = "5s sdf 5s sdf 5 dfdfs "
 # print(validate_message(message_json, user_input))  # True
 
 # Test pour isDate
 # message_json = {"isDate": True}
-# user_input = "papap 15/03/2024"
-# print(validate_message(message_json, user_input))  # True
-
-# Test pour sondage
-# message_json = {"sondage": {"1": "Option 1", "2": "Option 2"}}
-# user_input = "Option 1"
+# user_input = " 29/02/2026"
 # print(validate_message(message_json, user_input))  # True
 
 # Test pour isCreneau
 # message_json = {"isCreneau": True}
-# user_input = " qsdf 15/03/2024"
+# user_input = " qsdf 24:50 qsd"
 # print(validate_message(message_json, user_input))  # True
 
 # Test pour isSiege
@@ -108,12 +128,17 @@ def validate_message(message_json, user_input):
 
 # Test pour isQuantite
 # message_json = {"isQuantite": True}
-# user_input = "5"
+# user_input = "5 sdf sdf M sdsmf"
 # print(validate_message(message_json, user_input))  # True
 
 
 # Test avec plusieurs propriétés
 # message_json = {"isCreneau": True, "isGenre": True, "isSiege": True}
-# user_input = "genre : drama  creneau : 12/03/2022  siege : A56"
+# user_input = "genre : drama  creneau : 2:50 siege : A56"
+# print(validate_message(message_json, user_input))  # True
+
+# Test avec plusieurs propriétés Date et Number
+# message_json = {"isDate": True, "isNumber": True, "isSiege": True}
+# user_input = "date : 05/12/1990  creneau : 2 siege : A5"
 # print(validate_message(message_json, user_input))  # True
 
